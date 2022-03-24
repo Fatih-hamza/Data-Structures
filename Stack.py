c2bec143d65a7e94c0c9a9c2cddc8b1494c45bbd
Fatih-hamza <66041597+Fatih-hamza@users.noreply.github.com>
2022-03-24 13:39:29 +0100

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,36 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.items.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+
+
+
+
+
+
+
+
+

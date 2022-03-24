70cb650614f4e9984752f38eccd0c722fbea49bd
Fatih-hamza <66041597+Fatih-hamza@users.noreply.github.com>
2022-03-24 13:55:45 +0100

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,29 +1,29 @@
 class Stack:
 
-    def __init__(self, items=[]):
-        self.items = items.copy()
+    def __init__(self, values=[]):
+        self.values = values.copy()
 
     def is_empty(self):
-        return not self.items
+        return not self.values
 
     def push(self, value):
-        self.items.append(value)
+        self.values.append(value)
 
     def pop(self):
         if self.is_empty():
             return None
-        return self.items.pop()
+        return self.values.pop()
 
     def peek(self):
         if self.is_empty():
             return None
-        return self.items[-1]
+        return self.values[-1]
 
     def size(self):
-        return len(self.items)
+        return len(self.values)
 
     def __str__(self):
-        return str(self.items)
+        return str(self.values)
 
 
 
